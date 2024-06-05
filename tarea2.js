2071c4a248cbf5ce951823816b1f652e97005495
Jhos87 <163060256+Jhos87@users.noreply.github.com>
2024-06-04 20:51:03 -0600

    Update tarea2.js

--- tarea2.js
+++ tarea2.js
@@ -1,4 +1,6 @@
-document.querySelector('[placeholder="email"]').value = 'jhoselinesalazarorozco@gmail.com'; 
-document.querySelector('#password').value = 'jhosal394!'; 
-await new Promise(resolve => setTimeout(resolve, 5000)); 
-document.querySelector('#root > div > div > div > div > div > div > form > button').click();
+//page is https://rahulshettyacademy.com/AutomationPractice/
+document.querySelector("#checkBoxOption1").click();
+document.querySelector(".radioButton").click();
+$x("//fieldset[@class='pull-right']/input[@id='name']")[0].value = "jhos"; 
+setTimeout(function(){ console.log("2 seconds") }, 2000);
+$x("//fieldset[@class='pull-right']/input[@id='confirmbtn']")[0].click();
